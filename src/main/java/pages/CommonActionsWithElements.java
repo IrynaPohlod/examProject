8c9ca107155a11ba82eb27bd02582207d85b8b06
Iryna <pohlodiryna@gmail.com>
2022-09-09 21:34:32 +0300

    Added parent classes for pages and tests

---
+++ src/main/java/pages/CommonActionsWithElements.java
@@ -0,0 +1,28 @@
+package pages;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import static java.time.Duration.*;
+
+
+public class CommonActionsWithElements {
+
+    protected WebDriver webDriver;
+    Logger logger = Logger.getLogger(getClass());
+    protected WebDriverWait webDriverWait10, webDriverWait15;
+
+    public CommonActionsWithElements(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        PageFactory.initElements(webDriver, this);
+        webDriverWait10 = new WebDriverWait(webDriver, ofSeconds(10));
+        webDriverWait15 = new WebDriverWait(webDriver, ofSeconds(15));
+    }
+
+
+
+
+}
